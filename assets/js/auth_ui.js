d90d178fd2e5ed44bd8417f61b9a1c821ef4cf85
malsllami <44532530+malsllami@users.noreply.github.com>
2025-12-26 01:40:59 +0300

    Create auth_ui.js

---
+++ assets/js/auth_ui.js
@@ -0,0 +1,40 @@
+(function(){
+  const s = جلسة();
+
+  const navRegister = document.getElementById("navRegister");
+  const navMember = document.getElementById("navMember");
+  const navAdmin = document.getElementById("navAdmin");
+  const navLogout = document.getElementById("navLogout");
+
+  function hide(el){ if(el) el.style.display="none"; }
+  function show(el){ if(el) el.style.display=""; }
+
+  if(s.token){
+    hide(navRegister);
+    show(navLogout);
+
+    if(s.role==="مشترك"){
+      show(navMember);
+      hide(navAdmin);
+    }else if(s.role==="مدير"){
+      hide(navMember);
+      show(navAdmin);
+    }else{
+      // احتياط
+      show(navMember);
+      show(navAdmin);
+    }
+
+    if(navLogout){
+      navLogout.onclick = function(e){
+        e.preventDefault();
+        خروج();
+      };
+    }
+  }else{
+    show(navRegister);
+    hide(navLogout);
+    show(navMember);
+    show(navAdmin);
+  }
+})();
