99a812d9f5b3e17d007a04cc8070ccddf0f45f43
malsllami <44532530+malsllami@users.noreply.github.com>
2025-12-27 15:05:10 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxj142evTxOWoWhyTA-02zuuGwS-u_AKtRn6t-_7TEyr5pBhr25IuN84Q2_aXO8SEZYgw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbysxzBeG_0wHDDcfsipY2rEWs2iMXz2ON3K_Yr3NZ2qFF7jtg2oxZHHX8eVPox2xTyjQw/exec",
   SITE_NAME: "جمعية عائلية",
   WELCOME_TEXT: "موقع الجمعية العائلية نرحب بالمشتركين وننوهكم بان مدة الجمعية 10 اشهر وقيمة السهم 100 ريال والاستلام للسهم 1000 ريال"
 };
