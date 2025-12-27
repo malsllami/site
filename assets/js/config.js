2f9e2eed96eea9b618d8b37af6ce72e6d94b1367
malsllami <44532530+malsllami@users.noreply.github.com>
2025-12-28 02:30:43 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/PUT_DEPLOY_URL_HERE/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzrqd1KdWAMhwCW-67kLmUh-k2fKjdvd23QiQBMZ3TNZlOMN5zBt20WUWqwA_sYHf0V9w/exec"
 };
