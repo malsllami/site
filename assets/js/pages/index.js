9733e49b1a85d98743e46b8fb6f20e04b2e72410
malsllami <44532530+malsllami@users.noreply.github.com>
2025-12-28 03:36:35 +0300

    Update index.js

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,7 +1,17 @@
-(async function(){
+(function(){
+  loadSocieties();
+})();
+
+function escAttr(v){
+  return encodeURIComponent(String(v || ""));
+}
+
+async function loadSocieties(){
+  msg("", "");
+
   try{
-    const d = await get("قائمة الجمعيات");
-    const arr = d.جمعيات || [];
+    const data = await get("قائمة الجمعيات");
+    const arr = (data && data.جمعيات) ? data.جمعيات : [];
 
     if(!arr.length){
       setHtml("societies", "<div class='warn warn-gray'>لا توجد جمعيات</div>");
@@ -10,18 +20,14 @@
 
     setHtml("societies", arr.map(s => `
       <div class="soc" onclick="location.href='society.html?معرف=${escAttr(s.معرف)}'">
-        <b>${esc(s.اسم)}</b>
-        <div class="mt8">الحالة ${badge(s.حالة)}</div>
-        <div class="mt8">عدد المشتركين ${esc(s.عدد_المشتركين)}</div>
-        <div class="mt8">عدد الاسهم ${esc(s.عدد_الاسهم)}</div>
+        <b>${esc(s.اسم || "")}</b>
+        <div class="mt8">الحالة ${badge(s.حالة || "")}</div>
+        <div class="mt8">عدد المشتركين ${esc(s.عدد_المشتركين || 0)}</div>
+        <div class="mt8">عدد الاسهم ${esc(s.عدد_الاسهم || 0)}</div>
       </div>
     `).join(""));
 
   }catch(e){
-    msg("err", e.message);
+    msg("err", e.message || String(e));
   }
-})();
-
-function escAttr(s){
-  return encodeURIComponent(String(s || ""));
 }
